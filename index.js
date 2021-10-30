e380f044aafe8d3976f79efc95b4ac9b739036eb
Brooke Hellerich <brookeh815@cox.net>
2021-10-30 13:24:38 -0500

    commit tree 2c466552166d3272930a0ad9c725fe7e7247fb9c

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if(array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
@@ -9,7 +15,18 @@ function hasTargetSum(array, target) {
 /* 
   Add your pseudocode here
 */
+// make a function called hasTargetSum that checks to see if two numbers from
+// the array add up to a target sum. Ex: if array is [1,2,3,4] and target is 6,
+// i should return true, because two of the numbers, 2 & 4 in the array add up to 6.
+// if the target is 10, i should return false, as no numbers in the array add up to
+// 10.
 
+// hasTargetSum([22, 19, 4, 6, 30], 25)
+// iterate through each number in the array. for the current number, identify
+// complimentary number that adds to the target. compliment = target - current number
+// iterate through rest of array and check if any number is our compliment, if yes,
+// return true. If i get to the end of the array and have not found any numbers that
+// add to the target, return false
 /*
   Add written explanation of your solution here
 */
